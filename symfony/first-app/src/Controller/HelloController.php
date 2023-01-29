64c7ec79ad1237d780adbacea99658e931137c53
Daniel Saputra <danielwetan.io@gmail.com>
2023-01-29 21:10:12 +0700

    symfony rest controller

--- symfony/first-app/src/Controller/HelloController.php
+++ symfony/first-app/src/Controller/HelloController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class HelloController 
+class HelloController extends AbstractController
 {
 
   #[Route('/hello')]
@@ -15,5 +16,14 @@ class HelloController
     return new Response(
       '<html><body>Hello World</body></html>'
     );
+  }
+
+  #[Route('/profile')]
+  public function profile(): Response 
+  {
+    return $this->json([
+      'message' => 'Welcome to your new controller!',
+      'path' => 'src/Controller/HelloController.php',
+  ]);
   }
 }
