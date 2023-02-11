e712495de47e468999239752638d52ac0d8276ce
Daniel Saputra <danielwetan.io@gmail.com>
2023-02-11 12:03:19 +0700

    array spec assignment

---
+++ rspec/testing-ruby-with-rspec/section2.md
@@ -0,0 +1,3 @@
+
+Why is better to pass a class argument to the describe method instead of a string with the class name?
+To make implicit subject from the class argument
---
+++ rspec/testing-ruby-with-rspec/spec/array_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe Array do 
+  it 'should start off empty' do 
+    puts subject 
+    puts subject.class 
+    expect(subject.length).to eq(0)
+  end
+
+  it 'should contain an item' do 
+    subject.push("Something")
+    expect(subject.length).to eq(1)
+  end
+end
