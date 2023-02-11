841408202afcefc19f0b8887ed8661fade7dff7d
Daniel Saputra <danielwetan.io@gmail.com>
2023-02-11 13:34:23 +0700

    rspec explicit subject

---
+++ rspec/testing-ruby-with-rspec/spec/explicit_subject_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe Hash do  
+  # subject alias
+  subject(:bob) do 
+    { a: 1, b: 2 }
+  end
+
+  # let(:bob) { {a: 1, b: 2} }
+
+  it 'has two key-value pairs' do 
+    expect(subject.length).to eq(2)
+    expect(bob.length).to eq(2)
+  end
+
+  describe 'nested example' do 
+    it 'has two-key value pairs' do 
+      expect(subject.length).to eq(2)
+      expect(bob.length).to eq(2)
+    end
+  end
+end
